76e691f88c6a871b4fa2ea4d40ec5752aa0dc158
nantayo <77558823+nantayo@users.noreply.github.com>
2021-09-09 08:12:31 +0800

    Update diy-part2.sh

--- router/armvirt_64/diy-part2.sh
+++ router/armvirt_64/diy-part2.sh
@@ -20,8 +20,8 @@ sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/auto
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.3.254）
 sed -i 's/192.168.1.1/192.168.3.254/g' package/base-files/files/bin/config_generate
 
-# Modify default root's password（FROM 'password'[$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.] CHANGE TO 'Sora1924'）
-sed -i 's/root::0:0:99999:7:::/root:Sora1924:0:0:99999:7:::/g' /etc/shadow
+# Modify default root's password（FROM 'password'[$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.] CHANGE TO 'your password'）
+# sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
 
 # Replace the default software source
 # sed -i 's#openwrt.proxy.ustclug.org#mirrors.bfsu.edu.cn\\/openwrt#' package/lean/default-settings/files/zzz-default-settings
