74d5a584fb225240e701319b345d77c0e5fab100
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2021-09-29 10:50:07 +0800

    添加app，修改ip--192.168.1.188
    luci-app-amlogic，luci-app-godproxy，luci-app-adblock-plus，luci-app-openclash，luci-app-koolproxy

--- router/armvirt_64/diy-part2.sh
+++ router/armvirt_64/diy-part2.sh
@@ -7,36 +7,26 @@
 #========================================================================================================================
 
 # ------------------------------- Main source started -------------------------------
-#
-# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-argon）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+
 
 # Modify some code adaptation
 sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
+sed -i 's/services/system/g' package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua
 
 # Add autocore support for armvirt
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.3.254）
-sed -i 's/192.168.1.1/192.168.3.254/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.188/g' package/base-files/files/bin/config_generate
 #
 # ------------------------------- Main source ends -------------------------------
 
 # ------------------------------- Other started -------------------------------
 #
-# Add luci-theme-argon
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-svn co https://github.com/jerrykuku/luci-app-argon-config/trunk package/luci-app-argon-config
+git clone https://github.com/ophub/luci-app-amlogic.git package/lean/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-godproxy.git package/lean/luci-app-godproxy
+git clone https://github.com/small-5/luci-app-adblock-plus package/lean/luci-app-adblock-plus
+git clone https://github.com/vernesong/OpenClash package/lean/luci-app-openclash
+git clone https://github.com/wclmgcd/luci-app-koolproxy.git package/lean/luci-app-koolproxy
 
-# Add haproxy2.4.2
-rm -f package/feeds/packages/haproxy
-svn co https://github.com/cocokfeng/haproxy/trunk package/feeds/packages/haproxy
 
-# Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk package/luci-app-amlogic
-
-# Add luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/openwrt-passwall
-#
-# ------------------------------- Other ends -------------------------------
