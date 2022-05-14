8ec04798074d84ac1eace4b03e76d65373397f7e
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2022-05-14 13:44:14 +0800

    去除不需要的源

--- router/armvirt_64/diy-part1.sh
+++ router/armvirt_64/diy-part1.sh
@@ -16,7 +16,7 @@
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
 echo "src-git others https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
