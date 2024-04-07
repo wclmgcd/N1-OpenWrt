d00d5fbf6509774f2a5156eb72f542cf626391a3
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2024-04-07 20:30:08 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+
+#修改密码
+sed -i 's/^root:.*:/root:$1$q6Qf.IUu$Bd2tIMFHYYNOsmsIRBwHC0:19650:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Remove packages
 rm -rf feeds/luci/applications/luci-app-passwall
