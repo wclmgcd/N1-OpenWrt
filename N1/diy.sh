c620a7543a37dece8222a4d874182e16fd7f12d6
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2024-11-26 20:42:06 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -5,7 +5,7 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 
 #修改密码
 sed -i 's/^root:.*:/root:$1$q6Qf.IUu$Bd2tIMFHYYNOsmsIRBwHC0:19650:0:99999:7:::/g' package/base-files/files/etc/shadow
-git clone -b dev-md https://gitee.com/mindspore/mindscience.git
+
 # Remove packages
 # rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-homeproxy
