d580bb064817098b43e35d5a06868c76b2be41f4
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2024-11-26 10:15:38 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -9,8 +9,9 @@ sed -i 's/^root:.*:/root:$1$q6Qf.IUu$Bd2tIMFHYYNOsmsIRBwHC0:19650:0:99999:7:::/g
 # Remove packages
 # rm -rf feeds/luci/applications/luci-app-passwall
 # rm -rf feeds/luci/applications/luci-app-homeproxy
-git clone https://github.com/muink/homeproxy.git  package/luci-app-homeproxy
-
+# git clone https://github.com/muink/homeproxy.git  package/luci-app-homeproxy
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 # Add packages
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
