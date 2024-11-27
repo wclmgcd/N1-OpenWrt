6b6cf1bf7385f9531327b8dd05eadd679ab0f8a8
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2024-11-27 15:38:27 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,12 +9,12 @@ sed -i 's/^root:.*:/root:$1$q6Qf.IUu$Bd2tIMFHYYNOsmsIRBwHC0:19650:0:99999:7:::/g
 
 
 # Add luci-app-passwall2
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package-temp
-cp -r package-temp/* package/
-rm -rf package-temp
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package-temp
-mv -f package-temp/luci-app-passwall2 package/
-rm -rf package-temp
+# git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package-temp
+# cp -r package-temp/* package/
+# rm -rf package-temp
+# git clone https://github.com/xiaorouji/openwrt-passwall2.git package-temp
+# mv -f package-temp/luci-app-passwall2 package/
+# rm -rf package-temp
 
 git clone https://github.com/sbwml/luci-app-alist.git package-temp
 mv -f package-temp/luci-app-alist package/
