c311a2c3a5d121d5511317dbbebf3e20bb8ba298
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2026-04-30 11:52:42 +0800

    Add new package clones for luci-app-nikki and openwrt-clashoo

--- N1/diy.sh
+++ N1/diy.sh
@@ -16,7 +16,8 @@ git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/l
 
 rm -rf feeds/luci/applications/luci-app-homeproxy package/luci-app-homeproxy
 git clone -b dev/main https://github.com/muink/homeproxy.git package/luci-app-homeproxy
-
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/luci-app-nikki
+git clone https://github.com/kenzok8/openwrt-clashoo.git package/openwrt-clashoo
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 
 # 清理 gn 残留（非常重要）
